e3152dc5158dd1e255d0d7dfe70bef1391ef3f8b
GonzaloSantos1 <gsanore@gmail.com>
2022-03-04 14:46:26 +0100

    add iteration_1

---
+++ app.js
@@ -0,0 +1,19 @@
+//---------------- VARIABLES
+
+const clickBtn = document.querySelector('.btnToClick');
+const inputText = document.querySelector('.click');
+const inputFocus = document.querySelector('.focus');
+
+const inputInput = document.querySelector('.value');
+
+//---------------- EVENT LISTENERS
+clickBtn.addEventListener('click', function () {
+  console.log(inputText.value);
+});
+inputFocus.addEventListener('focus', function () {
+  console.log(inputFocus.value);
+});
+
+inputInput.addEventListener('input', function () {
+  console.log(inputInput.value);
+});
